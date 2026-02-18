76574f71e3438914d66a7d6dd7d30b2196d2b25f
ohtota <35125947+ohtota@users.noreply.github.com>
2026-02-18 09:21:56 +0900

    Add files via upload

--- 2026.1st/2026.1st.js
+++ 2026.1st/2026.1st.js
@@ -9,4 +9,40 @@ function chgTitleEN() {
     cols.forEach(col => col.style.setProperty("display", "none"));
   }
 }
-
+function setMode() {
+  const checkbox = document.getElementById("mode-checkbox");
+  let mode = null;
+  if (!window.matchMedia) {
+    mode = localStorage.getItem('theme');
+  } else if (isDarkMode()) {
+    mode = "dark"
+  }
+  if (mode === "dark") {
+    checkbox.checked = true;
+  } else {
+    checkbox.checked = false;
+  }
+  chgMode(checkbox);
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    if (event.matches) {
+      checkbox.checked = true;
+    } else {
+      checkbox.checked = false;
+    }
+    chgMode(checkbox);
+  });
+}
+function isDarkMode() {
+  return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+}
+function chgMode(checkbox) {
+  if (checkbox.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.documentElement.removeAttribute('data-theme');
+    if (localStorage.getItem('theme') !== null) {
+      localStorage.removeItem('theme');
+    }
+  }
+}
