d8535556964526d1b1d065e2e1e5382d6ff1d7b4
ohtota <ohtota@gmail.com>
2026-05-12 11:20:49 +0900

    配置変更と備考欄を追加

--- 2026.2nd/2026.2nd.js
+++ 2026.2nd/2026.2nd.js
@@ -1,23 +1,23 @@
 function chgTitleEN() {
-  const btn = document.getElementById("btn-title-en");
-  const cols = document.querySelectorAll("table.main tr > .hidden");
-  if (btn.textContent.includes("表示")) {
-    btn.textContent = btn.textContent.replace("表示", "消去");
-    cols.forEach(col => col.style.setProperty("display", "revert"));
+  const btn = document.getElementById('btn-title-en');
+  const cols = document.querySelectorAll('table.main tr > .hidden');
+  if (btn.textContent.includes('表示')) {
+    btn.textContent = btn.textContent.replace('表示', '消去');
+    cols.forEach(col => col.style.setProperty('display', 'revert'));
   } else {
-    btn.textContent = btn.textContent.replace("消去", "表示");
-    cols.forEach(col => col.style.setProperty("display", "none"));
+    btn.textContent = btn.textContent.replace('消去', '表示');
+    cols.forEach(col => col.style.setProperty('display', 'none'));
   }
 }
 function setMode() {
-  const checkbox = document.getElementById("mode-checkbox");
+  const checkbox = document.getElementById('mode-checkbox');
   let mode = null;
   if (!window.matchMedia) {
     mode = localStorage.getItem('theme');
   } else if (isDarkMode()) {
-    mode = "dark"
+    mode = 'dark'
   }
-  if (mode === "dark") {
+  if (mode === 'dark') {
     checkbox.checked = true;
   } else {
     checkbox.checked = false;
@@ -31,6 +31,7 @@ function setMode() {
     }
     chgMode(checkbox);
   });
+  setAlign();
 }
 function isDarkMode() {
   return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
@@ -45,4 +46,42 @@ function chgMode(checkbox) {
       localStorage.removeItem('theme');
     }
   }
+}
+function setAlign() {
+  let align = localStorage.getItem('align');
+  if (align !== 'L' && align !== 'C' && align !== 'R') {
+    align = 'C';
+  }
+  let id;
+  switch (align) {
+    case 'L': id = 'align-l'; break; 
+    case 'C': id = 'align-c'; break;
+    case 'R': id = 'align-r'; break;
+  }
+  document.getElementById(id).checked = true;
+  chgAlign();
+}
+function chgAlign() {
+  // alert('called chgAlign()');
+  const elems = document.getElementsByName('content-align');
+  let align = '';
+  let setting = '';
+  for (var i = 0; i < elems.length; i++) {
+    if (elems[i].checked === true) {
+      align = elems[i].value;
+      break;
+    }
+  }
+  if (align === '') {
+    return;
+  }
+  switch(align) {
+    case 'L': setting = '0'         ; break;
+    case 'C': setting = '0 auto'    ; break;
+    case 'R': setting = '0 0 0 auto'; break;
+  }
+  var elm = document.querySelector('div.wrapper');
+  elm.style.margin = setting;
+  localStorage.setItem('align', align);
+  // alert(setting);
 }
